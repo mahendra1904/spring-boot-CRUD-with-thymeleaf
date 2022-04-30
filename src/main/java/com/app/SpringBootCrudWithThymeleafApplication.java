529137ffea9db2606559e227e9ee479c54016680
mahendra1904 <mahendra1904@gmail.com>
2022-04-30 16:05:48 +0530

    curd

---
+++ src/main/java/com/app/SpringBootCrudWithThymeleafApplication.java
@@ -0,0 +1,15 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootCrudWithThymeleafApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootCrudWithThymeleafApplication.class, args);
+	}
+
+}
